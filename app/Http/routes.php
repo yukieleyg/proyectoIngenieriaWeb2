595fd50ba6fba0b9523bf9154ce7421107fc82e6
Jorge <jlts.01.94@gmail.com>
2016-09-12 22:50:49 -0600

    Laravel 5.2

---
+++ app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+//Route::get('/home', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/ejemplo', 'usuariosController@ejemplo');
+
+Route::get('/', 'principalController@index');
+
+Route::get('/home', 'principalController@home');
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,12 @@
+@extends('principal')
+@section('encabezado')
+	<h2>Inicio</h2>
+@stop
+
+@section('contenido')
+	<div class="jumbotron">
+	  <h1>Gestión de Proyectos</h1>
+	  <p>Este es un sistema que permite llevar un control de la gestión de proyectos de Software. Brindando los procesos más importantes para llevar a cabo un proyecto de calidad.</p>
+	  <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+	</div>
+@stop
